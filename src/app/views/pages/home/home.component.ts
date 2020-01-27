e28afc34e9c7fe0437aa72ad64269285d1042147
Calliope <sociality.dev4@gmail.com>
2020-01-27 10:58:45 +0200

    added pins to map

--- src/app/views/pages/home/home.component.ts
+++ src/app/views/pages/home/home.component.ts
@@ -1,13 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 	latitude = 38.262431;
 	longitude = 23.686613;
+	markers: marker[] = [
+	  {
+		  lat: 38.673858,
+		  lng: 23.815982,
+		  label: 'A',
+		  draggable: true
+	  },
+	  {
+		  lat: 38.373858,
+		  lng: 23.215982,
+		  label: 'B',
+		  draggable: false
+	  },
+	  {
+		  lat: 38.723858,
+		  lng: 23.015982,
+		  label: 'C',
+		  draggable: true
+	  }
+  ]
 
 	constructor() { }
 
