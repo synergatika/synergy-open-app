d995240c1cd4a0968b56cb462c12f78287483b78
Calliope <sociality.dev4@gmail.com>
2020-01-28 18:05:04 +0200

    offers, events

--- src/app/views/content/community-list/community-list.component.ts
+++ src/app/views/content/community-list/community-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadJsonService } from '../../../core/services/loadjson.service';
+
+// RxJS
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-community-list',
@@ -6,17 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./community-list.component.scss']
 })
 export class CommunityListComponent implements OnInit {
-	list = [
-		{
+	objectKeys = Object.keys;
+	list$: Observable<any>;
+	list1 = {
+		'Commonspace2103606333' : {
 			name: 'Commonspace',
 			img: './assets/media/images/uploaded/commonspace.webp',
 			sector: 'Recreation and Culture',
 			subscription_date: 'Jan 5, 2020',
-			email: 'nfo@commonspace.gr',
+			email: 'info@commonspace.gr',
 			phone: '2103606333',
 			address: 'Akakiou 1 - 3 & Ipeirou 60, 10439, Athens'
 		},
-		{
+		'Allois2103606333' : {
 			name: 'Syn Allois',
 			img: './assets/media/images/uploaded/synallois.jpg',
 			sector: 'Food',
@@ -25,7 +31,7 @@ export class CommunityListComponent implements OnInit {
 			phone: '2103606333',
 			address: 'Nileos 35, 11851, Athens'
 		},
-		{
+		'Sociality2103606333' : {
 			name: 'Sociality',
 			img: './assets/media/images/uploaded/sociallity.png',
 			sector: 'Durables (Technology)',
@@ -34,11 +40,17 @@ export class CommunityListComponent implements OnInit {
 			phone: '2103606333',
 			address: 'Solonos 136, 10677, Athens'
 		},
-	]
+	}
 	
-	constructor() { }
+	constructor(private loadData : LoadJsonService) { }
 
 	ngOnInit() {
+		this.loadData.getJSON('coops').subscribe(data => {			
+			//console.log('getJSON data');
+           // console.log(data);
+			this.list$ = of(data);
+
+        });
 	}
 
 }
