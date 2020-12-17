b63edf5eee30825819101ec43165b2161c3e1123
Grego Psonara <pastsar@gmail.com>
2020-12-17 17:44:09 +0200

    home banner and menu

---
+++ src/app/core/services/content-data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+/**
+ * Environments
+ */
+import { environment } from '../../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+//This service is used to get static data
+export class ContentService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  readContentById(content_id: string) {
+    return this.http.get<any>(`${environment.apiUrl}/content/${content_id}`)
+      .pipe(map(response => {
+        console.log(response.data);
+        return response.data;
+      }));
+  }
+
+}
