3f6b5af21104d9bb4789d7a8b21d905fe4ce9ec9
Grego Psonara <pastsar@gmail.com>
2021-01-12 02:48:21 +0200

    single event post ready

--- src/app/core/models/post_event.model.ts
+++ src/app/core/models/post_event.model.ts
@@ -4,6 +4,7 @@ export interface PostEvent {
   partner_name: string;
   subtitle?: string;
   partner_imageURL: string;
+  partner_slug:string;
 
   post_event_id: string;
   post_event_imageURL?: string;
