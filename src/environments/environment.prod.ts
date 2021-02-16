ffe7e92b9caf65d5770209c65b2a34a9dc0aba89
George Theofilis <g.theofilis@clmsuk.com>
2021-02-16 23:00:59 +0200

    Hide secret variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: true,
+    apiUrl: 'undefined',
+    appUrl: 'undefined',
+    mapApiKey: 'undefined',
+  
+    access: [
+        true, // community,
+        true, // loyalty,
+        true, // microcredit,
+        false // microfunding
+    ],
+    subAccess: [
+        true, //partner_address,
+        true, // partner_contact,
+        true, // partner_payments,
+        true, // partner_auto_registration,
+        true  // partner_fixed_campaign
+    ],
+  
+    version: 'undefined'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,36 @@
+import { secrets } from './secrets.environment.ts';
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  ...secrets,
+
+  production: false,
+  
+  access: [
+    true, // community,
+    true, // loyalty,
+    true, // microcredit,
+    false // microfunding
+  ],
+  subAccess: [
+    true, //partner_address,
+    true, // partner_contact,
+    true, // partner_payments, 
+    true, // partner_auto_registration, 
+    true  // partner_fixed_campaign
+  ],
+
+  version: 'undefined'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
