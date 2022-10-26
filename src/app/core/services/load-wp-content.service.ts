68e3cbfe654cd175c7697fe476db56036f152cce
Gregory <pastsar@gmail.com>
2022-10-26 19:31:11 +0300

    about page

--- src/app/core/services/load-wp-content.service.ts
+++ src/app/core/services/load-wp-content.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
+import { HttpClient } from '@angular/common/http';
 // RxJS
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -8,14 +8,12 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class LoadWpContentService {
-	resolveUrl = "https://wp.synergatika.gr/wp-json/wp/v2/";
-	
-  constructor(private http: HttpClient) { }	
+  resolveUrl = "https://wp.synergatika.gr/wp-json/wp/v2/";
 
-	getContent(pageId): Observable<any> {
-		return this.http.get<any>(this.resolveUrl + "./pages/"+ pageId).pipe(map(response => {
-			return response.content.rendered;
-		}));
-	}
-		
+  constructor(private http: HttpClient) { }
+
+  getContent(pageId): Observable<any> {
+    return this.http.get<any>(this.resolveUrl + "pages/" + pageId);
+  }
+
 }
